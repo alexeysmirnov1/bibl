616e2069515db669dfaa242fa9c4bf9ef7a1902d
smirnov <smirnovflag@gmail.com>
2025-07-31 23:59:50 +0500

    create book

---
+++ resources/views/bibl/create.blade.php
@@ -0,0 +1,27 @@
+<h1>My books</h1>
+
+<h1>Добавить книгу</h1>
+
+<form action="{{ route('bibl.books.store') }}" method="post">
+    <label>Название</label>
+    <input type="text" id="name">
+    <br>
+    <label>Автор</label>
+    <input type="text" id="author">
+    <br>
+    <label>Издание</label>
+    <input type="text" id="publisher">
+    <br>
+    <label>Жанр</label>
+    <input type="text" id="genre">
+    <br>
+    <label>Страницы</label>
+    <input type="text" id="pages">
+    <br>
+    <label>Статус</label>
+    <input type="text" id="status">
+    <br>
+    <label>Формат</label>
+    <input type="text" id="format">
+    <br>
+</form>
